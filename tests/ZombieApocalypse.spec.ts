e699de8fbfde5caeeef202f2f6a929122f3e1ec1
Mister Mister <myemail@gmail.com>
2020-06-19 14:49:46 +1000

    added more unit tests

--- tests/ZombieApocalypse.spec.ts
+++ tests/ZombieApocalypse.spec.ts
@@ -83,6 +83,162 @@ describe("ZombieApocalypse", () => {
     ]).to.have.deep.members(zombieApocalypse.getZombiesCoordinates());
   });
 
+  it("should infect acording to the rules. Simple Test 4.", () => {
+    //Arrange
+    const zombieApocalypse = new ZombieApocalypse(
+      1,
+      [{ x: 0, y: 0 }],
+      [{ x: 0, y: 0 }],
+      [Direction.U, Direction.D, Direction.L, Direction.R]
+    );
+
+    //Act
+    zombieApocalypse.startInfection();
+
+    //Assert
+    expect(1).to.equal(zombieApocalypse.getZombiesScore());
+    expect([
+      { x: 0, y: 0 },
+      { x: 0, y: 0 },
+    ]).to.have.deep.members(zombieApocalypse.getZombiesCoordinates());
+  });
+
+  it("should infect acording to the rules. Simple Test 5.", () => {
+    //Arrange
+    const zombieApocalypse = new ZombieApocalypse(
+      3,
+      [
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+        { x: 2, y: 0 },
+        { x: 0, y: 1 },
+        { x: 0, y: 2 },
+        { x: 1, y: 2 },
+        { x: 2, y: 2 },
+        { x: 2, y: 1 },
+      ],
+      [{ x: 1, y: 1 }],
+      [Direction.U, Direction.D, Direction.L, Direction.R]
+    );
+
+    //Act
+    zombieApocalypse.startInfection();
+
+    //Assert
+    expect(1).to.equal(zombieApocalypse.getZombiesScore());
+    expect([
+      { x: 0, y: 0 },
+      { x: 1, y: 0 },
+      { x: 2, y: 0 },
+      { x: 0, y: 1 },
+      { x: 0, y: 2 },
+      { x: 1, y: 2 },
+      { x: 2, y: 2 },
+      { x: 2, y: 1 },
+      { x: 1, y: 1 },
+    ]).to.have.deep.members(zombieApocalypse.getZombiesCoordinates());
+  });
+
+  it("should infect acording to the rules when zombie moves around but cannot reach any victim.", () => {
+    //Arrange
+    const zombieApocalypse = new ZombieApocalypse(
+      5,
+      [{ x: 2, y: 2 }],
+      [
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+        { x: 2, y: 0 },
+        { x: 3, y: 0 },
+        { x: 4, y: 0 },
+        { x: 0, y: 1 },
+        { x: 0, y: 2 },
+        { x: 0, y: 3 },
+        { x: 0, y: 4 },
+
+        { x: 1, y: 4 },
+        { x: 2, y: 4 },
+        { x: 3, y: 4 },
+        { x: 4, y: 4 },
+
+        { x: 4, y: 1 },
+        { x: 4, y: 2 },
+        { x: 4, y: 3 },
+      ],
+      [
+        Direction.D,
+        Direction.L,
+        Direction.U,
+        Direction.U,
+        Direction.R,
+        Direction.R,
+        Direction.D,
+        Direction.D,
+      ]
+    );
+
+    //Act
+    zombieApocalypse.startInfection();
+
+    //Assert
+    expect(0).to.equal(zombieApocalypse.getZombiesScore());
+    expect([{ x: 3, y: 3 }]).to.have.deep.members(
+      zombieApocalypse.getZombiesCoordinates()
+    );
+  });
+
+  it("should infect acording to the rules when no victims", () => {
+    //Arrange
+    const zombieApocalypse = new ZombieApocalypse(
+      5,
+      [{ x: 0, y: 0 }],
+      [],
+      [Direction.U, Direction.D, Direction.L, Direction.R]
+    );
+
+    //Act
+    zombieApocalypse.startInfection();
+
+    //Assert
+    expect(0).to.equal(zombieApocalypse.getZombiesScore());
+    expect([{ x: 0, y: 0 }]).to.have.deep.members(
+      zombieApocalypse.getZombiesCoordinates()
+    );
+  });
+
+  it("should infect acording to the rules when no zombies.", () => {
+    //Arrange
+    const zombieApocalypse = new ZombieApocalypse(
+      5,
+      [],
+      [{ x: 3, y: 4 }],
+      [Direction.U, Direction.D, Direction.L, Direction.R]
+    );
+
+    //Act
+    zombieApocalypse.startInfection();
+
+    //Assert
+    expect(0).to.equal(zombieApocalypse.getZombiesScore());
+    expect([]).to.have.deep.members(zombieApocalypse.getZombiesCoordinates());
+  });
+
+  it("should infect acording to the rules when no zombies and victims.", () => {
+    //Arrange
+    const zombieApocalypse = new ZombieApocalypse(
+      5,
+      [],
+      [],
+      [Direction.U, Direction.D, Direction.L, Direction.R]
+    );
+
+    //Act
+    zombieApocalypse.startInfection();
+
+    //Assert
+    expect(0).to.equal(zombieApocalypse.getZombiesScore());
+    expect([]).to.have.deep.members(zombieApocalypse.getZombiesCoordinates());
+  });
+
   it("should infect acording to the rules. Ailo's test.", () => {
     //Arrange
     const zombieApocalypse = new ZombieApocalypse(
